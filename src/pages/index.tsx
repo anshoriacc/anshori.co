a256d2e6061c2a659a33877c854442f8e29f0043
achmad anshori <utd.rik@gmail.com>
2022-08-29 13:36:02 +0700

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import HeadComponent from '@components/HeadComponent';
+import type {NextPage} from 'next';
+
+const Home: NextPage = () => {
+	return (
+		<>
+			<HeadComponent />
+			<h1>
+				<span>achmad</span>
+				<br />
+				<span>anshori/</span>
+			</h1>
+		</>
+	);
+};
+
+export default Home;
