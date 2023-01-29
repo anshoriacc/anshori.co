b888747f06cddd3571a6a2f090b1e99aa483ac37
achmad anshori <utd.rik@gmail.com>
2023-01-29 18:55:50 +0700

    update packages and config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          0: '#fafafa',
+          50: '#f0f0f0',
+          100: '#e1e1e1',
+          200: '#c8c8c8',
+          300: '#afafaf',
+          400: '#969696',
+          500: '#7d7d7d',
+          600: '#646464',
+          700: '#4b4b4b',
+          800: '#323232',
+          900: '#191919',
+          950: '#0f0f0f',
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
