ab90ffc0c2a6528ba25e7ba231660a3160db0ba0
achmad anshori <utd.rik@gmail.com>
2023-02-27 13:28:59 +0700

    add: loading bar at the top of the page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,37 @@
+import { useEffect, useRef } from 'react';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
+import { Router } from 'next/router';
+import LoadingBar from 'react-top-loading-bar';
 
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const ref = useRef<any>(null);
+
+  const startLoading = () => {
+    ref.current.continuousStart();
+  };
+
+  const endLoading = () => {
+    ref.current.complete();
+  };
+
+  useEffect(() => {
+    Router.events.on('routeChangeStart', startLoading);
+    Router.events.on('routeChangeComplete', endLoading);
+    Router.events.on('routeChangeError', endLoading);
+
+    return () => {
+      Router.events.off('routeChangeStart', startLoading);
+      Router.events.off('routeChangeComplete', endLoading);
+      Router.events.off('routeChangeError', endLoading);
+    };
+  }, []);
+
   return (
     <ThemeProvider attribute="class">
+      <LoadingBar color="#3b82f6" ref={ref} shadow={false} />
       <Component {...pageProps} />
     </ThemeProvider>
   );
