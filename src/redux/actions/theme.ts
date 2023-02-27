ba55f8a04e8c034d03918d37b84696edcd4f3e45
achmad anshori <utd.rik@gmail.com>
2023-02-27 11:17:25 +0700

    update: move theme (redux to next-themes)

--- src/redux/actions/theme.ts
+++
@@ -1,9 +0,0 @@
-import { Dispatch } from '../types';
-
-export const TOGGLE_THEME = 'TOGGLE_THEME';
-
-export const toggleTheme = () => (dispatch: Dispatch) => {
-  dispatch({
-    type: TOGGLE_THEME,
-  });
-};
--- src/redux/reducers/index.ts
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit';
-
-import theme from './theme';
-
-export default combineReducers({ theme });
