a03124c7fab6f487ae9d7d6fb40ea0d5a84e709d
arik <anshoriacc@gmail.com>
2023-11-05 22:37:34 +0700

    update: navbar

--- src/app/api/currently-playing/route.ts
+++ src/app/api/currently-playing/route.ts
@@ -1,5 +1,4 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
 
 import { getCurrentlyPlaying } from "@/services/spotifyApi";
 
@@ -10,20 +9,17 @@ export type TCurrentlyPlayingType = {
   songUrl?: string;
 };
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<TCurrentlyPlayingType>
-) {
+export async function GET() {
   const response = await getCurrentlyPlaying();
 
   if (response.status === 204 || response.status > 400) {
-    return res.status(200).json({ isPlaying: false });
+    return Response.json({ isPlaying: false });
   }
 
   const song = await response.data;
 
   if (song.item === null) {
-    return res.status(200).json({ isPlaying: false });
+    return Response.json({ isPlaying: false });
   }
 
   const isPlaying = song.is_playing;
@@ -33,5 +29,5 @@ export default async function handler(
     .join(", ");
   const songUrl = song.item.external_urls.spotify;
 
-  return res.status(200).json({ isPlaying, title, artist, songUrl });
+  return Response.json({ isPlaying, title, artist, songUrl });
 }
