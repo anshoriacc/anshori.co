95cf7bf4c5f118227d489774d28b0da156ed4e24
anshori <anshoriacc@gmail.com>
2025-02-02 12:31:14 +0700

    revert header

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,4 +1,4 @@
-// import { Header } from "@/components/header";
+import { Header } from "@/components/header";
 
 export default function HomeLayout({
   children,
@@ -6,9 +6,9 @@ export default function HomeLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="bg-neutral-800 text-neutral-50 cursor-default">
+    <div className="cursor-default bg-neutral-800 text-neutral-50">
       <div className="mx-auto min-h-dvh w-full max-w-2xl p-4 pt-[88px]">
-        {/* <Header /> */}
+        <Header />
 
         {children}
       </div>
---
+++ src/components/header.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+import {
+  AnimatePresence,
+  motion,
+  useMotionValueEvent,
+  useScroll,
+} from "motion/react";
+
+import { cn } from "@/lib/utils";
+
+export const Header = () => {
+  const { scrollY, scrollYProgress } = useScroll();
+  const [isActive, setIsActive] = React.useState(false);
+
+  useMotionValueEvent(scrollY, "change", latest => {
+    setIsActive(latest > 88);
+  });
+
+  return (
+    <motion.header
+      animate={{ width: isActive ? 136 : 96 }}
+      transition={{ type: "spring", duration: 0.5 }}
+      className={cn(
+        "fixed left-1/2 top-4 z-404 flex h-10 -translate-x-1/2 items-center justify-between gap-4 rounded-xl bg-black px-2 text-neutral-50 shadow",
+      )}>
+      <Link href="/" className="overflow-hidden">
+        <svg className="pointer-events-none h-5 w-20 fill-neutral-50">
+          <text
+            x={2}
+            y={17}
+            className={cn("stroke-2 text-xl font-black")}
+            paintOrder="stroke fill">
+            anshori
+            <tspan className="fill-red-500 stroke-none">.</tspan>
+          </text>
+        </svg>
+      </Link>
+
+      <AnimatePresence>
+        {isActive && (
+          <svg
+            viewBox="0 0 100 100"
+            className="size-6 -rotate-90 [&_circle]:fill-none [&_circle]:stroke-[16]">
+            <circle
+              cx="50"
+              cy="50"
+              r="40"
+              pathLength="1"
+              className="stroke-neutral-700"
+            />
+            <motion.circle
+              cx="50"
+              cy="50"
+              r="40"
+              className="stroke-neutral-50"
+              style={{ pathLength: scrollYProgress }}
+            />
+          </svg>
+        )}
+      </AnimatePresence>
+    </motion.header>
+  );
+};
