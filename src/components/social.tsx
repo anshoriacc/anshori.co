b2a2c9f1f884e495ae93a8d33c09622c18ac60be
anshori <anshoriacc@gmail.com>
2025-02-02 17:40:45 +0700

    update social

--- src/components/social.tsx
+++ src/components/social.tsx
@@ -15,15 +15,15 @@ export const Social = () => {
         "[&>a]:-m-4 [&>a]:flex [&>a]:aspect-square [&>a]:w-1/5 [&>a]:rotate-3 [&>a]:items-center [&>a]:justify-center [&>a]:rounded-2xl [&>a]:border-3 [&>a]:border-neutral-900 [&>a]:bg-neutral-800 [&>a]:p-4 [&>a]:transition-all [&>a]:duration-300 hover:[&>a]:m-0 hover:[&>a]:w-2/7 hover:[&>a]:rotate-0 hover:[&>a]:border-0",
         "hover:**:data-link:**:fill-neutral-500 hover:**:data-link:outline-neutral-500 has-[a:hover]:**:data-link:**:fill-neutral-50 has-[a:hover]:**:data-link:outline-neutral-50",
       )}>
-      <Link href="mailto:anshoriacc@gmail.com" className="z-3">
+      <Link href="mailto:anshoriacc@gmail.com" title="mail" className="z-3">
         <MailIcon className="size-3/4" />
       </Link>
 
-      <Link href="/github" target="_blank" className="z-2">
+      <Link href="/github" target="_blank" title="go to my github profile" className="z-2">
         <GithubIcon className="size-3/4" />
       </Link>
 
-      <Link href="/linkedin" target="_blank" className="z-1">
+      <Link href="/linkedin" target="_blank" title="go to my linkedin profile" className="z-1">
         <LinkedinIcon className="size-3/4" />
       </Link>
     </Card>
