1a08ef1b8f17ad4b5eff172453b97e4d9f33355c
anshori <anshoriacc@gmail.com>
2025-02-02 12:28:50 +0700

    update header

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,4 +1,4 @@
-import { Header } from "@/components/header";
+// import { Header } from "@/components/header";
 
 export default function HomeLayout({
   children,
@@ -8,7 +8,7 @@ export default function HomeLayout({
   return (
     <div className="bg-neutral-800 text-neutral-50 cursor-default">
       <div className="mx-auto min-h-dvh w-full max-w-2xl p-4 pt-[88px]">
-        <Header />
+        {/* <Header /> */}
 
         {children}
       </div>
